986aa56a32fe4f6f123d70f1891efd21dea20640
Vishal05-dev <vishalvenugopal00@gmail.com>
2026-03-02 11:12:28 +0530

    auto mail update feature added

--- pre-registration-ui/src/app/feature/demographic/demographic/demographic.component.ts
+++ pre-registration-ui/src/app/feature/demographic/demographic/demographic.component.ts
@@ -118,6 +118,8 @@ export class DemographicComponent
   config = {};
   consentMessage = [];
   titleOnError = "";
+  loggedInId= "";
+  loggedInEmail: boolean;
   dateOfBirthFieldId = "";
   isNavigateToDemographic = false;
   _moment = moment;
@@ -213,6 +215,15 @@ export class DemographicComponent
       if (this.isConsentMessage)
         this.consentDeclaration();
     }
+    this.loggedInId = localStorage.getItem("loginId");
+            console.log("logged in id", this.loggedInId);
+            let emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        //     this.transUserForm.patchValue({email: this.loggedInId})
+            if(emailRegex.test(this.loggedInId)){
+            this.userForm.patchValue({email: this.loggedInId})
+            }else{
+            this.userForm.patchValue({phone: this.loggedInId})
+            }
     this.onChangeHandler("");
     if (this.readOnlyMode) {
       this.userForm.disable();
